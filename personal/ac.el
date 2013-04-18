7a69d4c8474a5595dfc20add833596fddcc46732
Mark Baran <mark@gofastpay.com>
2013-04-17 17:48:53 -0700

    Autocomplete mode enabled.

---
+++ personal/ac.el
@@ -0,0 +1,17 @@
+(prelude-ensure-module-deps '(ac-nrepl))
+
+(require 'auto-complete-config)
+(ac-config-default)
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(require 'ac-nrepl)
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+
+(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
--- personal/settings.el
+++ personal/settings.el
@@ -1,4 +1,5 @@
 (set-default-font "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")
 (load-theme 'solarized-dark t)
+
 (setq prelude-guru nil)
 (setq prelude-whitespace nil)
