528ba4ac813b63bc2c4c5e5e583bba2026b821d5
Mark Baran <mark@gofastpay.com>
2013-12-02 11:07:36 -0800

    Updated Emacs configuration to reflect latest development in Emacs Clojure: CIDER.

--- personal/ac.el
+++ personal/ac.el
@@ -6,16 +6,17 @@
 (eval-after-load "auto-complete"
   '(progn
      (setq ac-auto-start nil)
-     (ac-set-trigger-key "TAB")
-     (add-to-list 'ac-modes 'nrepl-mode)))
+     (ac-set-trigger-key "TAB")))
 
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 (require 'ac-nrepl)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'cider-repl-mode))
+  
+(add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
--- personal/clojure.el
+++
@@ -1,7 +0,0 @@
-(prelude-ensure-module-deps '(nrepl-ritz))
-
-;; (setq nrepl-popup-stacktraces-in-repl t)
-
-(defun enable-ritz ()
-  (require 'nrepl-ritz))
-(add-hook 'nrepl-interaction-mode-hook 'enable-ritz)
