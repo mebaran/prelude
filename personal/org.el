92ba1fb7fc90a0dd08940f9153b6333cdb62c0f6
Mark Baran <mark@gofastpay.com>
2014-03-18 17:00:48 -0700

    Started personal org.el

---
+++ personal/org.el
@@ -0,0 +1,10 @@
+(require 'org)
+
+(add-to-list 'org-export-latex-classes
+             '("koma-article"
+               "\\documentclass{scrartcl}"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
