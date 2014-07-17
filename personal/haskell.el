1c22e1cd840ba9fc81c19dfc278bc8184c613b66
Mark Baran <mebaran@gmail.com>
2014-07-17 00:43:46 -0700

    Updated haskell.el to match new prelude.

--- personal/haskell.el
+++ personal/haskell.el
@@ -5,4 +5,4 @@
 (autoload 'ghc-init "ghc" nil t)
 
 (eval-after-load "haskell-mode"
-  '(add-hook 'haskell-mode-hook (lambda () (ghc-init))))
+  '(add-hook 'haskell-mode-hook (lambda () (interactive-haskell-mode))))
