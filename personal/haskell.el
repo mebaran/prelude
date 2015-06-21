6c539e4a065c859efab6b57c4143ee82c5cf0832
Mark Baran <mebaran@gmail.com>
2015-06-21 12:13:15 -0700

    Configured for my setup.

--- personal/haskell.el
+++
@@ -1,8 +0,0 @@
-(require 'prelude-haskell)
-
-(prelude-ensure-module-deps '(ghc ghci-completion))
-
-(autoload 'ghc-init "ghc" nil t)
-
-(eval-after-load "haskell-mode"
-  '(add-hook 'haskell-mode-hook (lambda () (interactive-haskell-mode))))
